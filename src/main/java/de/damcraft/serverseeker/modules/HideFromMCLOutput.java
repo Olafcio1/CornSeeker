bb2ea3ed8ef293f9d38a2585d7ddff64c14ca4db
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2024-08-28 21:33:20 +0200

    nothing important rlly opregionbypass not fully done

---
+++ src/main/java/de/damcraft/serverseeker/modules/HideFromMCLOutput.java
@@ -0,0 +1,17 @@
+package de.damcraft.serverseeker.modules;
+
+import de.damcraft.serverseeker.ServerSeeker;
+import meteordevelopment.meteorclient.events.game.SendMessageEvent;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+
+public class HideFromMCLOutput extends Module {
+    public HideFromMCLOutput() {
+        super(ServerSeeker.CATEGORY, "hide-from-mcl-output", "Hides all messages you send from the MCL output window of other players (MCL-21590).");
+    }
+
+    @EventHandler
+    public void onSendMessage(SendMessageEvent event) {
+        event.message = "]]>" + event.message;
+    }
+}
--- src/main/java/de/damcraft/serverseeker/modules/OPRegionBypass.java
+++ src/main/java/de/damcraft/serverseeker/modules/OPRegionBypass.java
@@ -1,11 +1,11 @@
 package de.damcraft.serverseeker.modules;
 
 import de.damcraft.serverseeker.ServerSeeker;
-import meteordevelopment.meteorclient.events.entity.player.PlaceBlockEvent;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.text.Text;
 
 public class OPRegionBypass extends Module {
     public OPRegionBypass() {
@@ -14,7 +14,14 @@ public class OPRegionBypass extends Module {
 
     @EventHandler
     public void onPacketSend(PacketEvent.Send e) {
-        if (e.packet instanceof PlayerInteractBlockC2SPacket)
-        e.cancel();
+        if (e.packet instanceof PlayerInteractBlockC2SPacket ev) {
+            e.cancel();
+            var info = ev.getBlockHitResult().getBlockPos();
+            int x = info.getX(),
+                y = info.getY(),
+                z = info.getZ();
+            ServerSeeker.mc.player.sendMessage(Text.of("seq:" + ev.getSequence()));
+            ServerSeeker.mc.player.networkHandler.sendCommand("/setblock " + x + " " + y + " " + z + " air");
+        }
     }
 }
