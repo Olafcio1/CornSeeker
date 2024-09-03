c7c7bed2e7112ccf1ec745519af975655c745d81
Olafcio <olafcio@int.pl>
2024-09-03 20:12:04 +0200

    uhm

---
+++ src/main/java/de/damcraft/serverseeker/modules/AutoGUIRules.java
@@ -0,0 +1,12 @@
+package de.damcraft.serverseeker.modules;
+
+import de.damcraft.serverseeker.ServerSeeker;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class AutoGUIRules extends Module {
+    public AutoGUIRules() {
+        super(ServerSeeker.C.MAIN, "AutoGUIRules", "Automatically clicks Accept on book GUIs.");
+    }
+
+    //idfk how to do this
+}
